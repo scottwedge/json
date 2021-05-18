e9ac598575009080a1e076e3be4ca1f238243e96
Scott Wedge <scott.wedge@gmail.com>
2021-05-17 20:44:08 -0400

    Initial commit

---
+++ sample1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Fooling around with JSON data formats
+
+# after 'import json', use json.dumps() to print formatted output
+# and json.loads() to read formatted input
+
+# Great documentation in help(json.dumps) and help(json.loads)
+
+
