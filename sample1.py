098ec1efb17952a8535c139194c591276083e2e8
Scott Wedge <scott.wedge@gmail.com>
2021-05-17 22:42:24 -0400

    Write dictionary and string in JSON format

--- sample1.py
+++ sample1.py
@@ -7,4 +7,26 @@
 
 # Great documentation in help(json.dumps) and help(json.loads)
 
+# Imports
+import json
+import time
+
+
+# Define variables
+file = "/tmp/t"
+d1 = {"oldest":"Scott", "youngest":"Allan"}
+l1 = [1, 2, 3, 4]
+s = "long_string"
+
+
+# Convert to JSON format
+j = json.dumps(d1, indent = 4)
+print(j)
+
+f = open(file, "w")
+f.write(j + "\n")
+f.write(s + "\n")
+f.close()
+
+
 
